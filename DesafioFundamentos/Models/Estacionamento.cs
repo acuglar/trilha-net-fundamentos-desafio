feec3db5e51b493270984ca7290be803cbc109ef
Alexandre Uglar <acuglar@gmail.com>
2023-12-30 03:55:50 -0300

    Implementa lógica da função RemoverVeiculo

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -34,30 +34,36 @@ namespace DesafioFundamentos.Models
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine();
+            string placaValida = VerificarPlaca(placa);
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
-
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (placaValida != null)
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                // Verifica se o veículo existe
+                if (veiculos.Any(x => x == placaValida))
+                {
+                    Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                    if (int.TryParse(Console.ReadLine(), out int horas))
+                    {
+                        decimal valorTotal = precoInicial + precoPorHora * horas;
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                        veiculos.Remove(placaValida);
+                        Console.WriteLine($"O veículo {placaValida} foi removido e o preço total foi de: R$ {valorTotal}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entrada inválida. Certifique-se de inserir um número inteiro.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                }
             }
             else
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine("Placa inválida. O formato deve estar LLLNNNN ou LLL-NNNN");
             }
         }
 
@@ -68,7 +74,7 @@ namespace DesafioFundamentos.Models
             {
                 string pluralIndicator = veiculos.Count == 1 ? "" : "s";
                 Console.WriteLine($"Há {veiculos.Count} veículo{pluralIndicator} estacionado{pluralIndicator}.");
-                
+
                 foreach (string veiculo in veiculos)
                 {
                     Console.WriteLine(veiculo);
